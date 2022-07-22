e83d1bcc2483fd25b532e778f1e46130cdc5b05d
GitHub Action <action@github.com>
2022-07-22 09:32:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/crazypino/pinoideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/crazypino/pinoideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.crazypino.pinoideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.crazypino.pinoideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
